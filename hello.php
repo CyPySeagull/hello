9692aa43c676dca1b03577eb0391599bd9b6f2e5
Anke S-F <ankesf@protonmail.com>
2021-09-14 12:55:38 +0200

    First tag for release

---
+++ hello.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title> Hello World! Seaside Glory </title>
+</head>
+
+<body>
+    <h1> Hello, new greetings with new version coming soon! </h1>
+    <?php echo "My name is Seagull, I am doing great"; ?>
+    <body>
+        </html>
